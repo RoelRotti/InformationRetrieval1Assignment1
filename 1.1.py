1bd949100f061084ccb6df6e84ae88c6ba122e90
RoelRotti <roel.rotteveel@gmail.com>
2021-02-02 17:46:10 +0100

    werkt

--- 1.1.py
+++ 1.1.py
@@ -1,27 +1,47 @@
-def download_dataset():
-    folder_path = os.environ.get("IR1_DATA_PATH")
-    if not folder_path:
-        folder_path = "./datasets/"
-    os.makedirs(folder_path, exist_ok=True)
-    
-    file_location = os.path.join(folder_path, "cacm.zip")
-    
-    # download file if it doesn't exist
-    if not os.path.exists(file_location):
-        
-        url = "https://surfdrive.surf.nl/files/index.php/s/M0FGJpX2p8wDwxR/download"
+# TODO: Implement this! (4 points)
+def read_cacm_docs(root_folder = "./datasets/"):
+    """
+        Reads in the CACM documents. The dataset is assumed to be in the folder "./datasets/" by default
+        Returns: A list of 2-tuples: (doc_id, document), where 'document' is a single string created by 
+            appending the title and abstract (separated by a "\n"). 
+            In case the record doesn't have an abstract, the document is composed only by the title
+    """
+    # YOUR CODE HERE
+    list_tuples = []
+    unique = 0
+    T = ""
+    W = ""
+    with open(root_folder+'cacm.all') as f:
+        line = f.readline()
+        while line:
+            read = 0
+            if line.startswith(".I"):
+                # If a T has been found before
+                if unique:
+                    list_tuples.append((id_tuple, T+W)) # Append what is known
+                    T=""
+                    W=""
+                id_tuple = int(line.strip(".I ").strip("\n")) # Extract last number string
+                unique = 1
+            if line.startswith(".T"):
+                # Extract the next couple of lines
+                line = f.readline()
+                while not line.startswith("."):
+                    T = T+line
+                    T = T.replace("\n", " ")
+                    line = f.readline()
+                    read = 1
+            if line.startswith(".W"):
+                # Extract next couple of lines
+                line = f.readline()
+                while not line.startswith("."):
+                    W = W+line
+                    W = W.replace("\n", " ")
+                    line = f.readline()
+                    read = 1
+            if not read: line = f.readline()
+        list_tuples.append((id_tuple, T+W)) # Append last one
+    #raise NotImplementedError()
+    return list_tuples
 
-        with open(file_location, "wb") as handle:
-            print(f"Downloading file from {url} to {file_location}")
-            response = requests.get(url, stream=True)
-            for data in tqdm(response.iter_content()):
-                handle.write(data)
-            print("Finished downloading file")
-    
-    if not os.path.exists(os.path.join(folder_path, "train.txt")):
-        
-        # unzip file
-        with zipfile.ZipFile(file_location, 'r') as zip_ref:
-            zip_ref.extractall(folder_path)
-        
-download_dataset()
+print(read_cacm_docs())
