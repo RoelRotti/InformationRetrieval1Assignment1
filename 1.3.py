1d57e43a1b95c6ecdf9d112faa29ff4710950cfc
RoelRotti <roel.rotteveel@gmail.com>
2021-02-02 17:30:39 +0100

    Create 1.3.py

---
+++ 1.3.py
@@ -0,0 +1,9 @@
+# TODO: Implement this! (1 point)
+def load_stopwords(root_folder = "./datasets/"):
+    """
+        Loads the stopwords. The dataset is assumed to be in the folder "./datasets/" by default
+        Output: A set of stopwords
+    """
+    # YOUR CODE HERE
+    raise NotImplementedError()
+
