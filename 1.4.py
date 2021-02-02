adab1e89acf220a521a45c60d999f9e0b82280f3
RoelRotti <roel.rotteveel@gmail.com>
2021-02-02 17:31:17 +0100

    Create 1.4.py

---
+++ 1.4.py
@@ -0,0 +1,9 @@
+# TODO: Implement this! (4 points)
+def tokenize(text):
+    """
+        Tokenizes the input text. Use the WordPunctTokenizer
+        Input: text - a string
+        Output: a list of tokens
+    """
+    # YOUR CODE HERE
+    raise NotImplementedError()
