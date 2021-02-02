d0c8c600cf4c7aaee0a60a00ebc033be61dfb95c
RoelRotti <roel.rotteveel@gmail.com>
2021-02-02 17:28:24 +0100

    Create 1.2.py

---
+++ 1.2.py
@@ -0,0 +1,9 @@
+# TODO: Implement this! (3 points)
+def read_queries(root_folder = "./datasets/"):
+    """
+        Reads in the CACM queries. The dataset is assumed to be in the folder "./datasets/" by default
+        Returns: A list of 2-tuples: (query_id, query)
+    """
+    # YOUR CODE HERE
+    raise NotImplementedError()
+
