bb7bb1c5b9506ee0010ef3d300bf191f156cbb54
RoelRotti <roel.rotteveel@gmail.com>
2021-02-02 18:25:59 +0100

    works

--- 1.2.py
+++ 1.2.py
@@ -5,5 +5,27 @@ def read_queries(root_folder = "./datasets/"):
         Returns: A list of 2-tuples: (query_id, query)
     """
     # YOUR CODE HERE
-    raise NotImplementedError()
-
+    list_tuples = []
+    unique = 0
+    W = ""
+    with open(root_folder+'query.text') as f:
+        line = f.readline()
+        while line:
+            read = 0
+            if line.startswith(".I"):
+                if unique:
+                    list_tuples.append((id_tuple, W)) # Append what is known
+                    W=""
+                id_tuple = int(line.strip(".I ").strip("\n")) # Extract last number string
+                unique = 1
+            if line.startswith(".W"):
+                line = f.readline()
+                while not line.startswith("."):
+                    W = W+line
+                    W = W.replace("\n", " ")
+                    line = f.readline()
+                    read = 1
+            if not read: line = f.readline()
+        list_tuples.append((id_tuple, W)) # Append last one
+    #raise NotImplementedError()
+    return list_tuples
