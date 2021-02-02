571978560e515d9fb56d3f6d3a1c357b863158fe
RoelRotti <roel.rotteveel@gmail.com>
2021-02-02 17:31:43 +0100

    Create 1.5.py

---
+++ 1.5.py
@@ -0,0 +1,9 @@
+# TODO: Implement this! (3 points)
+def stem_token(token):
+    """
+        Stems the given token using the PorterStemmer from the nltk library
+        Input: a single token
+        Output: the stem of the token
+    """
+    # YOUR CODE HERE
+    raise NotImplementedError()
